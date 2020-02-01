e97a511bd49b9ddf48ac2e1f04729068933641b9
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2020-02-01 22:35:30 +0530

    Initial Commit

---
+++ src/main/java/com/example/controller/MessageController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageController.class);
+    private final String message;
+
+    public MessageController(@Value("${message: Default Message}") String message) {
+        this.message = message;
+    }
+
+    @GetMapping("/message")
+    public String getMessage() {
+        LOGGER.info("Message : {}", message);
+        return message;
+    }
+}
