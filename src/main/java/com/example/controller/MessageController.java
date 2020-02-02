203403969a19f57326c92bfadc046646d3094e51
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2020-02-02 10:49:58 +0530

    Added Refresh Scope Annotation

--- src/main/java/com/example/controller/MessageController.java
+++ src/main/java/com/example/controller/MessageController.java
@@ -3,9 +3,11 @@ package com.example.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class MessageController {
 
